50c68f68aab2344a4e91046d371c5547048db950
myba <myassine.benammar@gmail.com>
2016-01-21 14:16:07 +0100

    Correctif ajout de version (Fichier resource dans composant2) (git add oublié dans l'ancien commit) Ajout composant3 et mise en place de sa version

---
+++ Composant3/Composant3.cpp
@@ -0,0 +1,25 @@
+// Composant2.cpp : Defines the routines for the DLL.
+//
+
+#include "Composant3.h"
+#include "Composant3Version.h"
+
+// declaration des fonctions internes
+int addition_interne(int a1, int a2);
+
+
+
+int composant3(int p1, int p2)
+{
+	return addition_interne(p1, p2);
+}
+
+int addition_interne(int a1, int a2)
+{
+	return a1 + a2;
+}
+
+char * getComposant3Version()
+{
+	return "Composant 3 version " COMPOSANT_VERSION_STR;
+}
